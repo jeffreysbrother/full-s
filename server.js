ad5140a2ac50233267e558b5dc52017c25dab0ec
James Cool <jeffreysbrother@gmail.com>
2017-01-04 15:22:49 -0800

    create server with express

--- server.js
+++ server.js
@@ -1,3 +1,16 @@
 import config from './config';
 
-console.log(config);
+import express from 'express';
+const server = express();
+
+server.get('/', (req, res) => {
+  res.send("hello Express");
+});
+
+server.get('/about', (req, res) => {
+  res.send("about page");
+});
+
+server.listen(config.port, () => {
+  console.info("Express listening on port ", config.port);
+});
