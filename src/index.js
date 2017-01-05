ea625b7b0ad674e457fa3e913e1aae50ebbff79b
James Cool <jeffreysbrother@gmail.com>
2017-01-04 16:11:23 -0800

    added some JSX

--- src/index.js
+++ src/index.js
@@ -1,7 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+// ReactDOM.render(
+//   React.createElement('h2', null, 'Hello React'),
+//   document.getElementById('root')
+// );
+
 ReactDOM.render(
-  React.createElement('h2', null, 'Hello React'),
+  <h2>Hello React with JSX!</h2>,
   document.getElementById('root')
 );
